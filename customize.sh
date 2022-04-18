86e452ac1fdb0d5b1fa1229c38b49a08300c876f
Zhao Ru Wei <934566509@qq.com>
2022-04-19 00:07:59 +0800

    XIAOZHAO-PLUS

--- customize.sh
+++ customize.sh
@@ -13,4 +13,4 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 rm openwrt/package/lean/luci-theme-argon -rf
-git clone -b 18.https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
